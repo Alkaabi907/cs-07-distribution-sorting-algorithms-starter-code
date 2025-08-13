11c88ffb5cab97b2bd05dc7e2c456bd7b49366af
AlkAlkaabi907 <Alkaabi454@gmail.com>
2025-08-13 16:43:06 +0300

    Bucket

--- bucket-sort.js
+++ bucket-sort.js
@@ -1,6 +1,44 @@
-function bucketSort(items) {
+function bucketSort(array, bucketSize = 5, compareFn = (a, b) => a - b) {
+  if (!Array.isArray(array) || array.length === 0) {
+    return [];
+  }
 
-  return items;
+  // Find min and max values
+  let minValue = Math.min(...array);
+  let maxValue = Math.max(...array);
+
+  // Determine bucket count
+  let bucketCount = Math.floor((maxValue - minValue) / bucketSize) + 1;
+  let buckets = Array.from({ length: bucketCount }, () => []);
+
+  // Distribute elements into buckets
+  for (let value of array) {
+    let bucketIndex = Math.floor((value - minValue) / bucketSize);
+    buckets[bucketIndex].push(value);
+  }
+
+  // Sort each bucket and concatenate results
+  let sortedArray = [];
+  for (let bucket of buckets) {
+    insertionSort(bucket, compareFn);
+    sortedArray.push(...bucket);
+  }
+
+  return sortedArray;
+}
+
+function insertionSort(array, compareFn) {
+  for (let i = 1; i < array.length; i++) {
+    let currentValue = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && compareFn(array[j], currentValue) > 0) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = currentValue;
+  }
+  return array;
 }
 
 module.exports = bucketSort;
